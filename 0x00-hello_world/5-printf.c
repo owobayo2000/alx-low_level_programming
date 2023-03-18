aabe7dbe6948a07fd39dee1a431f5ea6eab27e55
owobayo2000 <owobayo2000@gmaile.com>
2023-03-18 16:17:30 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	print("with proper grammar, but the outcome is a piece of act,\n");
-	return (0);
+    printf("%s%c", "with proper grammar, but the outcome is a piece of art", '\n');
+    return (0);
 }
