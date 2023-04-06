37290f74d4c77052d10489a6347e3ab8313fe461
owobayo2000 <owobayo2000@gmail.com>
2023-04-05 20:18:42 -0700

    Prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * check - checks to see if the number is prime
+ * @a: integer a
+ * @b: intger b
+ * Return: int
+ */
+
+int check(int a, int b)
+{
+	if (b < 2 || b % a == 0)
+		return (0);
+	else if (a > b / 2)
+		return (1);
+	else
+		return (check(a + 1, b));
+}
+
+/**
+ * is_prime_number - Function that states if number is prime
+ * @n: integer
+ * Return: integer
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (check(2, n));
+}
