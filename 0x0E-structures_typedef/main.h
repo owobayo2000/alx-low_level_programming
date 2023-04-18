71e11f5360e4ec5da4795c60279131176a6166a6
owobayo2000 <owobayo2000@gmail.com>
2023-04-17 18:54:51 -0700

    Init_dog

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -5,8 +5,8 @@
  * Header file prototypes for 0x0E-structures_typedef
  */
 struct dog;
-typedef struct dog dog_t;
 
+typedef struct dog dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
